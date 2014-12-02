0ee703d470ba6798a6d024c2af68c2b50b28dd99
optics2 <optics2.0@gmail.com>
2014-12-01 19:38:10 -0500

    stuff

--- Project/Project2/Main.js
+++ Project/Project2/Main.js
@@ -594,7 +594,7 @@ Main.prototype.generateTunnelGeometry2 = function (vertices, segments, radius, r
 
 Main.prototype.createTunnelMesh2 = function (geom, color, ambient) {
 
-    var texture = THREE.ImageUtils.loadTexture("http://dc603.4shared.com/img/uc_f0HRL/s7/13fd1b75360/water.jpg");
+    var texture = THREE.ImageUtils.loadTexture("http://joat.es/images/water.jpg");
     texture.wrapT = THREE.RepeatWrapping;
     texture.wrapS = THREE.RepeatWrapping;
     texture.repeat.set(numpoints / 2, 1);
@@ -618,7 +618,7 @@ Main.prototype.createTunnelMesh2 = function (geom, color, ambient) {
 }
 Main.prototype.createpath = function (geom) {
 
-    var texture = THREE.ImageUtils.loadTexture("http://dc603.4shared.com/img/uc_f0HRL/s7/13fd1b75360/water.jpg");
+    var texture = THREE.ImageUtils.loadTexture("http://joat.es/images/water.jpg");
     texture.wrapT = THREE.RepeatWrapping;
     texture.wrapS = THREE.RepeatWrapping;
     texture.repeat.set( numpoints/2, 1 );
@@ -640,7 +640,7 @@ Main.prototype.createpath = function (geom) {
 
 Main.prototype.createTunnelMesh = function (geom) {
 
-    var texture = THREE.ImageUtils.loadTexture("http://dc603.4shared.com/img/uc_f0HRL/s7/13fd1b75360/water.jpg");
+    var texture = THREE.ImageUtils.loadTexture("http://joat.es/images/water.jpg");
     texture.wrapT = THREE.RepeatWrapping;
     texture.wrapS = THREE.RepeatWrapping;
     texture.repeat.set( numpoints/2, 1 );
