efdc846b44ccf8ff021173adce9d7b3b24a59ecc
optics2 <optics2.0@gmail.com>
2014-12-01 19:49:58 -0500

    image change

--- Project/Project2/Main.js
+++ Project/Project2/Main.js
@@ -281,7 +281,7 @@ numpoints = 90;
 
     for (var fu = 0; fu < maxballs; fu++) {
         var particleGroup, particleAttributes;
-        var particleTexture = THREE.ImageUtils.loadTexture('http://www.nckas.org/images/objects/polaris.jpg');
+        var particleTexture = THREE.ImageUtils.loadTexture('http://i252.photobucket.com/albums/hh35/optics2/spark.png');
 
         particleGroup = new THREE.Object3D();
         particleAttributes = { startSize: [], startPosition: [], randomness: [] };
@@ -594,7 +594,7 @@ Main.prototype.generateTunnelGeometry2 = function (vertices, segments, radius, r
 
 Main.prototype.createTunnelMesh2 = function (geom, color, ambient) {
 
-    var texture = THREE.ImageUtils.loadTexture("http://www.mms3d.cn/kapok3d/textures/img/water.jpg");
+    var texture = THREE.ImageUtils.loadTexture("http://i252.photobucket.com/albums/hh35/optics2/water.jpg");
     texture.wrapT = THREE.RepeatWrapping;
     texture.wrapS = THREE.RepeatWrapping;
     texture.repeat.set(numpoints / 2, 1);
@@ -618,7 +618,7 @@ Main.prototype.createTunnelMesh2 = function (geom, color, ambient) {
 }
 Main.prototype.createpath = function (geom) {
 
-    var texture = THREE.ImageUtils.loadTexture("http://www.mms3d.cn/kapok3d/textures/img/water.jpg");
+    var texture = THREE.ImageUtils.loadTexture("http://i252.photobucket.com/albums/hh35/optics2/water.jpg");
     texture.wrapT = THREE.RepeatWrapping;
     texture.wrapS = THREE.RepeatWrapping;
     texture.repeat.set( numpoints/2, 1 );
@@ -640,7 +640,7 @@ Main.prototype.createpath = function (geom) {
 
 Main.prototype.createTunnelMesh = function (geom) {
 
-    var texture = THREE.ImageUtils.loadTexture("http://www.mms3d.cn/kapok3d/textures/img/water.jpg");
+    var texture = THREE.ImageUtils.loadTexture("http://i252.photobucket.com/albums/hh35/optics2/water.jpg");
     texture.wrapT = THREE.RepeatWrapping;
     texture.wrapS = THREE.RepeatWrapping;
     texture.repeat.set( numpoints/2, 1 );
